b56899c5fef08702b6137cb7f0a96139eef6090d
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-15 14:42:34 -0500

    hide healthbar

--- scripts/mondays/MondaySongManager.hx
+++ scripts/mondays/MondaySongManager.hx
@@ -8,6 +8,9 @@ class MondaySongManager
 {
 	public static function songStuffs()
 	{
+		PlayState.instance.healthBarBG.visible = false;
+		PlayState.instance.healthBar.visible = false;
+		
 		PlayState.instance.comboPopUps.offsets = [510, 320];
 
 		var opponentStrumline:FlxSprite = PlayState.instance.opponentStrumline;
