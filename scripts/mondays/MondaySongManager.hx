429d9e5223f0b5e1d91d697adb49317157c627e9
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-15 14:18:44 -0500

    hxformat

--- scripts/mondays/MondaySongManager.hx
+++ scripts/mondays/MondaySongManager.hx
@@ -1,12 +1,8 @@
 package mondays;
 
-import funkin.play.PlayState;
-
 class MondaySongManager
 {
 	public static function enableMiddleScroll()
 	{
-		var game:PlayState = PlayState.instance;
-		
 	}
 }
