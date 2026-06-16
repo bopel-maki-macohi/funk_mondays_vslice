820a058b19b7b39a0b3859a753f69889bb181e72
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-15 19:30:17 -0500

    spooky place stage

--- scripts/mondays/stages/MondayVoidStage.hx
+++ scripts/mondays/stages/MondayVoidStage.hx
@@ -1,5 +1,6 @@
 package scripts.mondays.stages;
 
+import flixel.util.FlxColor;
 import funkin.Paths;
 import funkin.graphics.FunkinSprite;
 import funkin.play.PlayState;
@@ -18,9 +19,26 @@ class MondayVoidStage extends Stage
 		return 'funk_mondays/stages/$path';
 	}
 
+	function makeBaseStageProp(img:String)
+	{
+		var prop:StageProp = new StageProp();
+		prop.loadTexture(getPath(img));
+		prop.active = false;
+		return prop;
+	}
+
+	var void:StageProp;
+
 	function buildStage()
 	{
 		super.buildStage();
+
+		void = new StageProp();
+		void.makeSolidColor(Std.int(FlxG.width * 1.1), Std.int(FlxG.height * 1.1), FlxColor.fromString('0xFFFFFF'));
+		void.scale.set(FlxG.width * 1.1, FlxG.height * 1.1);
+		void.antialiasing = false;
+		void.active = false;
+		addProp(void, 'stage');
 
 		var game = PlayState.instance;
 
@@ -28,6 +46,8 @@ class MondayVoidStage extends Stage
 
 		switch (game.currentSong.songName)
 		{
+			case 'Spooky Down South':
+				buildSpookyPlace();
 			default:
 				// case 'MrDearest':
 				buildMondayStage();
@@ -36,19 +56,38 @@ class MondayVoidStage extends Stage
 		refresh();
 	}
 
+	function buildSpookyPlace()
+	{
+		this._data.cameraZoom = 0.85;
+
+		var floor:StageProp = makeBaseStageProp('spookyPlace/floor');
+		floor.scale.set(1.5, 1.5);
+		floor.setPosition(0, 725);
+		addProp(floor, 'floor');
+
+		var window:StageProp = makeBaseStageProp('spookyPlace/window');
+		// window.scale.set(1.5, 1.5);
+		window.setPosition(0, 50);
+		addProp(window, 'floor');
+
+		var staircase:StageProp = makeBaseStageProp('spookyPlace/staircase');
+		staircase.scale.set(2, 2);
+		staircase.setPosition(1180, 500);
+		staircase.alpha = 0.5;
+		staircase.zIndex = 400;
+		staircase.scrollFactor.set(0.5, 0.5);
+		addProp(staircase, 'floor');
+	}
+
 	function buildMondayStage()
 	{
 		this._data.cameraZoom = 0.8;
 
-		var stage:StageProp = new StageProp();
-		stage.loadTexture(getPath('mondayStage/stage'));
-		stage.active = false;
+		var stage:StageProp = makeBaseStageProp('mondayStage/stage');
 		stage.setPosition(60, 725);
 		addProp(stage, 'stage');
 
-		var curtains:StageProp = new StageProp();
-		curtains.loadTexture(getPath('mondayStage/curtains'));
-		curtains.active = false;
+		var curtains:StageProp = makeBaseStageProp('mondayStage/curtains');
 		curtains.scale.set(2, 2);
 		curtains.scrollFactor.set(0.5, 0.5);
 		curtains.zIndex = 400;
