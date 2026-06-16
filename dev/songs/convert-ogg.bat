95ac5a9e3c8515586dc6d967e729a4f91175612a
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-06-15 22:34:11 -0500

    convert og file

--- dev/songs/convert-ogg.bat
+++ dev/songs/convert-ogg.bat
@@ -1,6 +1,7 @@
 cd dev
 cd songs
 
+ffmpeg -i Assasination.wav Assasination.ogg
 ffmpeg -i Assasination-inst.wav Assasination-inst.ogg
 ffmpeg -i Assasination-bf.wav Assasination-bf.ogg
 ffmpeg -i Assasination-sisian.wav Assasination-sisian.ogg
